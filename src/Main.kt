fd0d3ac2c8fea16d7d9b2f9a7da7dfd8cd735e16
lovebigc0c <jarickneror@gmail.com>
2024-01-19 09:54:16 +0300

    hi

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+fun main (){
+    var x = readLine()?.toDoubleOrNull()!!
+    var b = readLine()!!
+    var y = readLine()!!.toDouble()
+    if  (b == "+") {
+        var a = x + y
+        println(a)
+    }
+    else if (b == "-"){
+        var a = x - y
+        println(a),f
+    }
+    else if (b == "*"){
+        var a = x * y
+        println(a)
+    }
+    else if (b == "/"){
+        var a = x / y
+        println(a)
+    }
+}
